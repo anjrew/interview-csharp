2e3047d394192de00b2f14589c82d1912f75da28
Andrew Johnson <earyzhe@gmail.com>
2023-02-13 19:00:51 +0100

    Add more code to try and fix the test case

--- Application.tests/UnitTest1.cs
+++ Application.tests/UnitTest1.cs
@@ -1,8 +1,10 @@
-using System.Reflection;
-using UrlShortenerService.Application.Url;
 using UrlShortenerService.Application.Common.Interfaces;
+using UrlShortenerService.Application.Url.Commands;
+using UrlShortenerService.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 using HashidsNet;
 using Moq;
+
 namespace Application.tests;
 
 public class UnitTest1
@@ -10,13 +12,30 @@ public class UnitTest1
     [Fact]
     public async Task Test1()
     {
+
+        Func<string, string> getExpected = idHash => $"http://localhost:5246/u/{idHash}";
+        int id = 1;
+        string mock_hash = "erf22";
+        string expected = getExpected(mock_hash);
+
         // IApplicationDbContext context, IHashids hashids
         var context = new Mock<IApplicationDbContext>();
         var hashids = new Mock<IHashids>();
-        context.Setup(x => x.Hashids).Returns(hashids.Object);
+
+        var mockSet = new Mock<DbSet<Url>>();
+        context.Setup(x =>  x.SaveChangesAsync(CancellationToken.None)).Returns(Task.FromResult(id));
+        context.Setup(x =>  x.Urls).Returns(mockSet.Object);
+        hashids.Setup(x =>  x.Encode()).Returns(mock_hash);
+
         CreateShortUrlCommandHandler handler  = new CreateShortUrlCommandHandler(context.Object, hashids.Object);
-        var result =  await handler.Handle("https://www.google.com", CancellationToken.None);
-        Assert.Equal(result, "https://www.google.com");
+
+        var request = new CreateShortUrlCommand() {
+            Url = "http://www.google.com"
+        };
+
+        var result =  await handler.Handle(request, CancellationToken.None);
+
+        Assert.Equal(expected, result);
 
     }
 }
